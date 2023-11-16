a7ad9470475933acc84d91a636cb5402b2b082df
Alex Ostle <aos28@uclive.ac.nz>
2023-11-16 17:26:38 +1300

    Start on index page for desktop version

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+"""Main module for alex-personal-site Flask app"""
+
 import os
-from flask import *
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """Render index page"""
     return render_template('index.html')
+
+@app.route('/info')
+def info():
+    """Render info page"""
+    return render_template('info.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
